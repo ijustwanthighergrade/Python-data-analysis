7e37d18dc65b22a7e7a09fcb833de96092d39c16
ijustwanthighergrade <gill998546@gmail.com>
2023-12-24 04:35:06 +0800

    更新

--- DataAnalysis/apriori.py
+++ DataAnalysis/apriori.py
@@ -2,26 +2,16 @@ import pandas as pd
 from apyori import apriori
 import os
 
-# 读取数据
 script_dir = os.path.dirname(os.path.abspath(__file__))
 age = os.path.join(script_dir, "collect.xlsx")
-
-# 指定要读取的非连续行的索引（行索引从0开始）
 selected_columns = [1, 2, 3, 4, 5, 6]
-
-# 读取特定列的数据
 df = pd.read_excel(age, usecols=selected_columns)
-
-
-df = df.astype(str)  # Use this line for replacement
-
-# 将数据转换为列表
+df = df.astype(str)  
 data_list = df.values.tolist()
-# 输出数据列表
-# print(data_list)
+print(data_list)
 
 # 使用apriori算法
-results = list(apriori(data_list, min_support=0.1, min_confidence=0.1, min_lift=1.5, max_length=2))
+results = list(apriori(data_list, min_support=0.1, min_confidence=0.1, min_lift=1.5, max_length=5))
 
 # print(results)
 # 
--- README.md
+++ README.md
@@ -3,9 +3,10 @@
 pip install -r requirements.txt
 
 ## To make sure you can run django :
-Please check your floder is on "\Python-data-analysis\analysis"
-You can use "cd flodername" to move your position.
-After you check it, please run: 
+Please check your folder is on "\Python-data-analysis\analysis"
+You can use "cd foldername" to move your folder position.
+After you check it, please run:   
+
 python manage.py runserver
 
 Then you can see the website.
