0a41d94d42779a399eb1a23ee19637fbb1886e31
Ivan06144 <150783493+Ivan06144@users.noreply.github.com>
2023-12-30 13:40:11 +0800

    Add files via upload

---
+++ kmeans/kmeanss.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 30 13:37:46 2023
+
+@author: j9103
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.cluster import KMeans
+from mpl_toolkits.mplot3d import Axes3D
+
+
+file_path = '/mnt/data/房價與結婚意願之相關分析_1214修改版 的副本 (回覆) (1).xlsx'
+df = pd.read_excel(file_path)
+
+
+# Mapping age ranges to numeric values
+age_mapping = {'21～29': 25, '30～39': 35, '40～49': 45, '50～59': 55, '60以上': 65}
+df['年齡數值'] = df['您的年齡'].map(age_mapping)
+
+# Mapping salary ranges to numeric values
+salary_mapping = {'0～19999': 10000, '20000～39999': 30000, '40000～59999': 50000, '60000～79999': 70000, '80000以上': 90000}
+df['月薪資數值'] = df['目前月薪資水平(台幣)'].map(salary_mapping)
+
+# Selecting relevant features for K-means
+features = ['年齡數值', '月薪資數值', '請問買房影響您結婚意願的程度為？']
+data_for_clustering = df[features].dropna()
+
+# Applying K-means clustering
+kmeans = KMeans(n_clusters=3, random_state=0)
+clusters = kmeans.fit_predict(data_for_clustering)
+
+# Adding cluster information to the dataframe
+data_for_clustering['Cluster'] = clusters
+
+correlation_matrix = data_for_clustering.corr()
+plt.figure(figsize=(10, 8))
+sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', fmt=".2f")
+plt.title('Heatmap of Correlation Between Age, Monthly Salary, and Influence on Marriage Intention')
+plt.show()
+"""
+熱力圖，其中明確標示了 x 軸和 y 軸的標籤。這個圖顯示了「年齡」、「月薪資水平」以及「對買房影響結婚意願的程度」之間的相關性。相關係數以不同顏色表示
+，幫助我們直觀地看到不同特徵之間的關聯強度。
+"""
